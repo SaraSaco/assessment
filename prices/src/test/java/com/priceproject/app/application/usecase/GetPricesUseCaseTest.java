ea753102c5d3c338125bc21b9d3f5a812b747d58
sarasaco <sarasacob@gmail.com>
2023-10-30 02:25:05 +0100

    test: Added tests

--- prices/src/test/java/com/priceproject/app/application/usecase/GetPricesUseCaseTest.java
+++ prices/src/test/java/com/priceproject/app/application/usecase/GetPricesUseCaseTest.java
@@ -20,25 +20,16 @@ public class GetPricesUseCaseTest {
 	@Autowired
 	private GetPricesUseCase useCase;
 	private String date;
-	@Autowired
-	private static PriceService ps;
-	private static int productId;
-	private static int brandId;
+
+	private static Integer productId;
+	private static Integer brandId;
 	private double priceExpected; 
 	private  LocalDateTime startDateExpected ;
 	private  LocalDateTime endDateExpected ;
+	private Integer price_list;
 	
 	@BeforeAll
 	public static void onload() {
-//		GetPricesUseCase uc = new GetPricesUseCase();
-//		try {
-//			uc.getClass().getDeclaredField("priceService").set(ps, new PriceService());
-//		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		ps = new PriceService();
 		productId = 35455;
 		brandId = 1;
 	}
@@ -52,12 +43,16 @@ public class GetPricesUseCaseTest {
 		priceExpected = 35.50;
 		startDateExpected = LocalDateTime.of(2020, 6, 14, 0, 0, 0);
 		endDateExpected = LocalDateTime.of(2020, 12, 31, 23, 59, 59);
+		price_list = 1;
 		
 		PriceOutput po = (PriceOutput)useCase.execute(date, productId, brandId);		
 	
 		assertTrue(priceExpected == po.getPrice());
 		assertTrue(startDateExpected.equals(po.getStartDate()));
 		assertTrue(endDateExpected.equals(po.getEndDate()));
+		assertTrue(productId.equals((po.getProductId())));
+		assertTrue(brandId.equals((po.getBrandId())));
+		assertTrue(price_list.equals(po.getPrice_list()));
 	}
 	
 	@Test
@@ -68,32 +63,76 @@ public class GetPricesUseCaseTest {
 		priceExpected = 25.45;
 		startDateExpected = LocalDateTime.of(2020, 6, 14, 15, 0, 0);
 		endDateExpected = LocalDateTime.of(2020, 6, 14, 18, 30, 0);
+		price_list = 2;
 		
 		PriceOutput po = (PriceOutput)useCase.execute(date, productId, brandId);		
 	
 		assertTrue(priceExpected == po.getPrice());
 		assertTrue(startDateExpected.equals(po.getStartDate()));
 		assertTrue(endDateExpected.equals(po.getEndDate()));
+		assertTrue(productId.equals((po.getProductId())));
+		assertTrue(brandId.equals((po.getBrandId())));
+		assertTrue(price_list.equals(po.getPrice_list()));
 	}
 	
 	@Test
 	public void test3() {
-		fail("Not yet implemented");
+		date="2020-06-14-21.00.00";
+		
+		// Expected output
+		priceExpected = 35.50;
+		startDateExpected = LocalDateTime.of(2020, 6, 14, 0, 0, 0);
+		endDateExpected = LocalDateTime.of(2020, 12, 31, 23, 59, 59);
+		price_list = 1;
+		
+		PriceOutput po = (PriceOutput)useCase.execute(date, productId, brandId);		
+	
+		assertTrue(priceExpected == po.getPrice());
+		assertTrue(startDateExpected.equals(po.getStartDate()));
+		assertTrue(endDateExpected.equals(po.getEndDate()));
+		assertTrue(productId.equals((po.getProductId())));
+		assertTrue(brandId.equals((po.getBrandId())));
+		assertTrue(price_list.equals(po.getPrice_list()));
 	}
 	
 	@Test
 	public void test4() {
-		fail("Not yet implemented");
+		date="2020-06-15-10.00.00";
+		
+		// Expected output
+		priceExpected = 30.50;
+		startDateExpected = LocalDateTime.of(2020, 6, 15, 0, 0, 0);
+		endDateExpected = LocalDateTime.of(2020, 6, 15, 11, 0, 0);
+		price_list = 3;
+		
+		PriceOutput po = (PriceOutput)useCase.execute(date, productId, brandId);		
+	
+		assertTrue(priceExpected == po.getPrice());
+		assertTrue(startDateExpected.equals(po.getStartDate()));
+		assertTrue(endDateExpected.equals(po.getEndDate()));
+		assertTrue(productId.equals((po.getProductId())));
+		assertTrue(brandId.equals((po.getBrandId())));
+		assertTrue(price_list.equals(po.getPrice_list()));
 	}
 	
 	@Test
 	public void test5() {
-		fail("Not yet implemented");
+		date="2020-06-16-21.00.00";
+		
+		// Expected output
+		priceExpected = 38.95;
+		startDateExpected = LocalDateTime.of(2020, 6, 15, 16, 0, 0);
+		endDateExpected = LocalDateTime.of(2020, 12, 31, 23, 59, 59);
+		price_list = 4;
+		
+		PriceOutput po = (PriceOutput)useCase.execute(date, productId, brandId);		
+	
+		assertTrue(priceExpected == po.getPrice());
+		assertTrue(startDateExpected.equals(po.getStartDate()));
+		assertTrue(endDateExpected.equals(po.getEndDate()));
+		assertTrue(productId.equals((po.getProductId())));
+		assertTrue(brandId.equals((po.getBrandId())));
+		assertTrue(price_list.equals(po.getPrice_list()));
 	}
 	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
 }
