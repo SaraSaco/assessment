07f14e59674d612a125f795f03faf00505e6a9cb
sarasaco <sarasacob@gmail.com>
2023-10-30 01:31:24 +0100

    feat: configuration for database

---
+++ prices/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO price_rates(brand_id, start_date, end_date, price_list_id, product_id, priority, price, currency) VALUES (1, TIMESTAMP '2020-06-14 00:00:00', TIMESTAMP '2020-12-31 23:59:59', 1, 35455, 0, 35.50, 0);
+
+INSERT INTO price_rates(brand_id, start_date, end_date, price_list_id, product_id, priority, price, currency) VALUES (1, TIMESTAMP '2020-06-14 15:00:00', TIMESTAMP '2020-06-14 18:30:00', 2, 35455, 1, 25.45, 0);
+
+INSERT INTO price_rates(brand_id, start_date, end_date, price_list_id, product_id, priority, price, currency) VALUES (1, TIMESTAMP '2020-06-15 00:00:00', TIMESTAMP '2020-06-15 11:00:00', 3, 35455, 1, 30.50, 0);
+
+INSERT INTO price_rates(brand_id, start_date, end_date, price_list_id, product_id, priority, price, currency) VALUES (1, TIMESTAMP '2020-06-15 16:00:00', TIMESTAMP '2020-12-31 23:59:59', 4, 35455, 1, 38.95, 0);
