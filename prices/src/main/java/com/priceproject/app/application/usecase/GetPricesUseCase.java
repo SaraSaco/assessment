ebeb246fbd9607d1b15f610db15a35488e3d5289
sarasaco <sarasacob@gmail.com>
2023-11-03 15:11:43 +0100

    refactor(GetPricesUseCase): Update GetPricesUseCase

--- prices/src/main/java/com/priceproject/app/application/usecase/GetPricesUseCase.java
+++ prices/src/main/java/com/priceproject/app/application/usecase/GetPricesUseCase.java
@@ -1,16 +1,15 @@
 package com.priceproject.app.application.usecase;
 
-import java.time.DateTimeException;
+
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.priceproject.app.domain.model.ErrorOutput;
 import com.priceproject.app.domain.model.Output;
 
 import com.priceproject.app.domain.service.PriceService;
-import com.priceproject.app.infrastructure.DateParse;
+
 
 @Component
 public class GetPricesUseCase {
@@ -18,15 +17,9 @@ public class GetPricesUseCase {
 	@Autowired
 	private PriceService priceService;
 	
-	public Output execute(String date, Integer productId, Integer brandId){
-		DateParse dateParse = new DateParse();
-		Output o;
-		try {
-			LocalDateTime d = dateParse.ParseStringToLocalDateTime(date);
-			o = priceService.findPriceByDateAndProductIdAndBrand(d, productId, brandId);
-		} catch(DateTimeException dte) {
-			o = new ErrorOutput("Incorrect date format was introduced.");
-		}
+	public Output execute(LocalDateTime date, Integer productId, Integer brandId){
+		
+		Output o = priceService.findPriceByDateAndProductIdAndBrand(date, productId, brandId);;
 		return o;
 	}
 
